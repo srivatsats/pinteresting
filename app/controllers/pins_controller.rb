5487803c653384a83cdc22e24cafae4a28ffe930
Srivatsa Sreenivasarao <srivatsats@gmail.com>
2014-07-06 22:59:34 +0530

    Incorporating abiity to delete

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :pin_owner, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :destrot, :edit, :update]
 
   def index
     @pins = Pin.all
